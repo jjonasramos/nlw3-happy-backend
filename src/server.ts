0dad9a4760a9eb908f8f77f367dcb704b5469dfc
Jonas Ramos <jjonasramos@hotmail.com>
2020-10-13 23:38:37 -0300

    Part1 Backend - 57:37

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,26 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
 
 const app = express();
+
+app.use(express.json());
+
+app.post('/orphanages', async (req, res) => {
+
+    const { name, latitude, longitude, about, instructions, opening_hours, open_on_weekends } = req.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+        name, latitude, longitude, about, instructions, opening_hours, open_on_weekends
+    });
+
+    await orphanageRepository.save(orphanage);
+
+    return res.json({ message: 'Hello World' });
+});
 
 app.listen(3333);
